6705c44fae7fbcd8cee69aa661bdeffc0e46f42d
Toshit Chawda <r58playz@gmail.com>
2024-04-08 01:20:08 -0700

    add pty support

--- twisp/src/main.rs
+++ twisp/src/main.rs
@@ -1,7 +1,9 @@
-use std::{error::Error, ffi::OsString, net::SocketAddr, result};
+mod pty;
+
+use std::{error::Error, ffi::OsString, net::SocketAddr, path::PathBuf, result};
 
 use bytes::Bytes;
-use clap::Parser;
+use clap::{Args, Parser};
 use fastwebsockets::{upgrade, FragmentCollectorRead, WebSocketError};
 use futures_util::TryFutureExt;
 use http_body_util::Empty;
@@ -14,13 +16,26 @@ use wisp_mux::{CloseReason, ConnectPacket, MuxStream, ServerMux, StreamType};
 
 type Result<T> = std::result::Result<T, Box<dyn Error + Sync + Send>>;
 
+/// Wisp protocol server that exposes PTYs over the Wisp connection.
 #[derive(Debug, Parser)]
+#[command(version = clap::crate_version!())]
 struct Cli {
-    #[clap(short, long)]
-    bind: SocketAddr,
+    #[clap(flatten)]
+    backend: Backend,
 }
 
-async fn handle_mux(connect: ConnectPacket, mut stream: MuxStream) -> Result<()> {
+#[derive(Debug, Args)]
+#[group(required = true, multiple = false)]
+pub struct Backend {
+    /// PTY device to multiplex over
+    #[arg(short, long)]
+    pty: Option<PathBuf>,
+    /// Socket to bind to
+    #[arg(short, long)]
+    bind: Option<SocketAddr>,
+}
+
+async fn handle_muxstream(connect: ConnectPacket, mut stream: MuxStream) -> Result<()> {
     if connect.stream_type == StreamType::Tcp {
         let mut stream = stream.into_io().into_asyncrw();
         let mut pty = Pty::new()?;
@@ -44,19 +59,12 @@ async fn handle_mux(connect: ConnectPacket, mut stream: MuxStream) -> Result<()>
     Ok(())
 }
 
-async fn handle(fut: upgrade::UpgradeFut) -> Result<()> {
-    let (rx, tx) = fut.await?.split(tokio::io::split);
-    let rx = FragmentCollectorRead::new(rx);
-
-    let (mut server, fut) = ServerMux::new(rx, tx, u32::MAX);
-
-    tokio::spawn(fut);
-
+async fn handle_mux(mut server: ServerMux) -> Result<()> {
     while let Some((connect, stream)) = server.server_new_stream().await {
         tokio::spawn(async move {
             let mut close_err = stream.get_close_handle();
             let mut close_ok = stream.get_close_handle();
-            let _ = handle_mux(connect, stream)
+            let _ = handle_muxstream(connect, stream)
                 .or_else(|err| async move {
                     let _ = close_err.close(CloseReason::Unexpected).await;
                     Err(err)
@@ -65,14 +73,24 @@ async fn handle(fut: upgrade::UpgradeFut) -> Result<()> {
                 .await;
         });
     }
-
     Ok(())
 }
 
-async fn upgrade(req: Request<Incoming>) -> result::Result<Response<Empty<Bytes>>, WebSocketError> {
+async fn handle_ws(fut: upgrade::UpgradeFut) -> Result<()> {
+    let (rx, tx) = fut.await?.split(tokio::io::split);
+    let rx = FragmentCollectorRead::new(rx);
+
+    let (server, fut) = ServerMux::new(rx, tx, u32::MAX);
+
+    tokio::spawn(fut);
+
+    handle_mux(server).await
+}
+
+async fn upgrade_ws(req: Request<Incoming>) -> result::Result<Response<Empty<Bytes>>, WebSocketError> {
     let (response, fut) = upgrade::upgrade(req)?;
     tokio::spawn(async move {
-        if let Err(err) = handle(fut).await {
+        if let Err(err) = handle_ws(fut).await {
             error!("Failed to handle connection: {:?}", err);
         }
     });
@@ -84,20 +102,31 @@ async fn main() -> Result<()> {
     env_logger::init();
     let args = Cli::parse();
 
-    let listener = TcpListener::bind(&args.bind).await?;
-    info!("Server started on {}", args.bind);
+    if let Some(bind) = args.backend.bind {
+        let listener = TcpListener::bind(&bind).await?;
+        info!("Server started on {}", bind);
 
-    while let Ok((stream, addr)) = listener.accept().await {
-        info!("{} connected", addr);
-        tokio::spawn(async move {
-            let io = TokioIo::new(stream);
-            let fut = http1::Builder::new()
-                .serve_connection(io, service_fn(upgrade))
-                .with_upgrades();
-            if let Err(err) = fut.await {
-                error!("Failed to serve connection to {}: {:?}", addr, err);
-            }
-        });
+        while let Ok((stream, addr)) = listener.accept().await {
+            info!("{} connected", addr);
+            tokio::spawn(async move {
+                let io = TokioIo::new(stream);
+                let fut = http1::Builder::new()
+                    .serve_connection(io, service_fn(upgrade_ws))
+                    .with_upgrades();
+                if let Err(err) = fut.await {
+                    error!("Failed to serve connection to {}: {:?}", addr, err);
+                }
+            });
+        }
+    } else if let Some(pty) = args.backend.pty {
+        let (rx, tx) = pty::open_pty(&pty).await?;
+        let (mux, fut) = ServerMux::new(rx, tx, u32::MAX);
+
+        tokio::spawn(fut);
+
+        handle_mux(mux).await?;
+    } else {
+        unreachable!("neither bind nor pty specified");
     }
     Ok(())
 }
